edb2cb1f9b8ada3a5ac9582f5bc68be466016dcf
TerenYeung <yangjialong@ifanr.com>
2018-02-07 09:43:24 +0800

    change icon to dynamic switching

--- assets/lib/handler.js
+++ assets/lib/handler.js
@@ -2,24 +2,27 @@ require(["gitbook", "jquery"], function(gitbook, $) {
   function handler(e, config) {
     var toolTipMode = config['anchor-navigation-ex'].toolTipMode;
     function handlerClickTooltip() {
-      var $toolTip = $('#anchor-navigation-ex-navbar')
-      var $list = $toolTip.find('ul')
+      var $toolTip = $('#anchor-navigation-ex-navbar .fa')
+      var $list = $('#anchor-navigation-ex-navbar ul')
 
       $toolTip.on('click', function() {
+        $toolTip.toggleClass('fa-plus-circle fa-times-circle')
         $list.toggleClass('tooltip-show')
       })
     }
     
     function handlerHoverTooltip() {
-      var $toolTip = $('#anchor-navigation-ex-navbar')
-      var $list = $toolTip.find('ul')
+      var $toolTip = $('#anchor-navigation-ex-navbar .fa')
+      var $list = $('#anchor-navigation-ex-navbar ul')
 
       $toolTip.on('mouseenter', function() {
         $list.addClass('tooltip-show')
+        $toolTip.addClass('fa-plus-circle')
       })
     
       $toolTip.on('mouseleave', function() {
         $list.removeClass('tooltip-show')
+        $toolTip.removeClass('fa-plus-circle')
       })
     }
 
@@ -30,13 +33,8 @@ require(["gitbook", "jquery"], function(gitbook, $) {
     }
   }
 
-  gitbook.events.bind('start', function (e) {
-    var config = gitbook.state.config.pluginsConfig
-    handler(e, config)
-  })
-
-  gitbook.events.bind('page.change', function (e) {
-    var config = gitbook.state.config.pluginsConfig
-    handler(e, config)
-  })
+    gitbook.events.bind('page.change', function (e) {
+      var config = gitbook.state.config.pluginsConfig
+      handler(e, config)
+    })
 })
